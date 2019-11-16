4b8868906f84970d3376ca9bef8f3d57966a7435
Harshad-Chavan <harshadchavan141@gmail.com>
2019-11-16 21:33:36 +0530

    new client file

--- client.py
+++ client.py
@@ -6,10 +6,19 @@ s = socket.socket()
 
 # Define the port on which you want to connect
 port = 80
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
 # connect to the server on local computer
 s.connect(('127.0.0.1', port))
+s.send(IPAddr.encode())
+reply = s.recv(1024).decode()
+print(reply)
+while True:
+    x = input("enter some text:" + "\n")
+    s.send(x.encode())
+    reply = s.recv(1024).decode()
+    print(reply)
+    if reply == 'bye':
+        break
 
-# receive data from the server
-print(s.recv(1024))
-# close the connection
 s.close()
